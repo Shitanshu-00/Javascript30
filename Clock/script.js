0aa1e988fc69900c87e6b8bf3c41bd196f7665cb
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-09-08 20:08:39 +0530

    Clock App

--- Clock/script.js
+++ Clock/script.js
@@ -14,7 +14,8 @@ const setDate=()=>{
     const minDegrees = ((minutes/60)*360)+90;
     minHand.style.transform = `rotate(${minDegrees}deg)`;
 
-    const hourDegrees = ((hours/60)*360)+90;
+    const hourDegrees = ((hours/12)*360)+90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
 };
 setInterval(setDate, 1000);
