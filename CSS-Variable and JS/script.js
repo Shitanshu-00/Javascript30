f48ac69fa82f98ae7c2ac77c76675f710e95afe2
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-09-14 22:00:07 +0530

    Update CSS Variable

--- CSS-Variable and JS/script.js
+++ CSS-Variable and JS/script.js
@@ -1,3 +1,9 @@
-const func =()=>{
-    console.log("Hello");
-}
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdate (){
+  const suffix = this.dataset.sizing || '';
+  document.documentElement.style.setProperty(`--${this.name}`, this.value+ suffix);
+};
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
