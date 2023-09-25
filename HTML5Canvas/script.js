e9917740825e482cc9f2dc6736a05f6ac705e06a
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-09-25 23:22:49 +0530

    HTML Canvas

--- HTML5Canvas/script.js
+++ HTML5Canvas/script.js
@@ -1,1 +1,43 @@
-console.log('Canvas');
+const canvas = document.querySelector("#draw");
+
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#ffdd00";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 0;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+let direction = true;
+
+const draw = (e) => {
+  if (!isDrawing) return; //stop fn from running when they are not moused down
+
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+
+  hue >= 360 ? hue = 0 : hue ++;
+
+  ctx.lineWidth >= 500 || ctx.lineWidth <= 1 ? direction = !direction: direction;
+
+  direction ? ctx.lineWidth++ : ctx.lineWidth--;
+};
+
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
