aea4a1dacb4039ee418e51cbb50573323993cbb1
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-09-29 19:58:25 +0530

    Dev Tools

--- DevTools/script.js
+++ DevTools/script.js
@@ -3,22 +3,23 @@ const dogs = [
   { name: "Snickers", age: 2 },
   { name: "Hugo", age: 8 },
 ];
+
 const makeGreen = () => {
   const p = document.querySelector("p");
   p.style.color = "#00aa00";
   p.style.fontSize = "50px";
 };
 
-//Regular
+// Regular
 console.log("Hello");
 
-//Interpolated
+// Interpolated
 console.log("Hello I am a %s string!", "😊");
 
 let val = "❤";
 console.log(`Hello I am a ${val} string!`);
 
-//Styled
+// Styled
 console.log(
   "%c I am some great text",
   "font-size:50px; background:red; text-shadow: 10px 10px 0 blue"
@@ -27,25 +28,53 @@ console.log(
 // Warning
 console.warn("OH NOOO...");
 
-//Error
+// Error
 console.error(`It's an error!`);
 
-//Info
+// Info
 console.info("Crocodiles eat 3-4 peoples per year");
 
-//Testing
+// Testing
 const p = document.querySelector("p");
 console.assert(p.classList.contains("ouch"), "Does not contain class ouch"); //Triggers only when case fails
 
-//Clearing
+// Clearing
 console.clear();
 
-//Viewing DOM Elements
+// Viewing DOM Elements
 console.log(p);
 console.dir(p); //displays an interactive list of the properties of the specified JavaScript object
 
-//Grouping together
+// Grouping together
 dogs.forEach((dog) => {
-   console.log(`${dog.name} is ${dog.age} years old`);
-   console.log(`${dog.name} is ${dog.age*7} years old`);
+  // console.group(`${dog.name}`)
+  console.groupCollapsed(`${dog.name}`);
+  console.log(`${dog.name} is ${dog.age} years old`);
+  console.log(`${dog.name} is ${dog.age * 7} years old`);
+  console.groupEnd(`${dog.name}`);
 });
+
+// Counting
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Steve');
+console.count('Wes');
+console.count('Steve');
+console.count('Wes');
+console.count('Wes');
+console.count('Steve');
+
+// Timing
+console.time('fetching data');
+fetch('https://api.github.com/users/Shitanshu-00')
+   .then(data => data.json())
+   .then(data => {
+      console.timeEnd('fetching data');
+      console.log(data);
+   });
+
+// Table
+console.table(dogs);
